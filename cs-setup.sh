3d095860080797dbca2cdb98da8ccec4c027410d
Alexey Alekhin <alexey.alekhin@tapad.com>
2020-10-26 04:17:55 +0100

    script cleanup

--- cs-setup.sh
+++ cs-setup.sh
@@ -1,24 +1,27 @@
 echo '::group::Install Coursier'
 curl -fLo cs https://git.io/coursier-cli-linux &&
-chmod +x cs &&
-./cs
+chmod +x cs
+echo "::set-output name=cs-version::$(./cs --version)"
 echo '::endgroup::'
 
 echo '::group::Install JVM'
+JVM=""
 if [ "${{ inputs.jvm }}" ]; then
-  ./cs java --env ${{ inputs.jvm }}
+    JVM="--jvm ${{ inputs.jvm }}"
 fi
-./cs java-home
-echo "JAVA_HOME=$(./cs java-home)" >> $GITHUB_ENV
+if [ -z "$JVM" ] && [ "$JAVA_HOME" ]; then
+    echo "skipping, JVM is already installed in $JAVA_HOME"
+else
+    ./cs java $JVM -version
+fi
+echo "JAVA_HOME=$(./cs java-home $JVM)" >> $GITHUB_ENV
 echo '::endgroup::'
 
 echo '::group::Install Apps'
 if [ "${{ inputs.apps }}" ]; then
-  export COURSIER_BIN_DIR=${{ github.action_path }}/apps
-  echo "COURSIER_BIN_DIR=$COURSIER_BIN_DIR" >> $GITHUB_ENV
-  echo "$COURSIER_BIN_DIR" >> $GITHUB_PATH
-  ./cs install ${{ inputs.apps }}
+    COURSIER_BIN_DIR=${{ github.action_path }}/apps
+    echo "COURSIER_BIN_DIR=$COURSIER_BIN_DIR" >> $GITHUB_ENV
+    echo "$COURSIER_BIN_DIR" >> $GITHUB_PATH
+    ./cs install --dir $COURSIER_BIN_DIR ${{ inputs.apps }}
 fi
 echo '::endgroup::'
-
-echo "::set-output name=cs-version::$(./cs --version)"
