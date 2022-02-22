8dd25b2b70351e6bbb7c9c6430ad756cf556f804
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2022-02-22 12:41:07 +0100

    Update coursier to 2.1.0-M5

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as os from 'os'
 import * as path from 'path'
 import * as tc from '@actions/tool-cache'
 
-const csVersion = '2.1.0-M3'
+const csVersion = '2.1.0-M5'
 const coursierVersionSpec = csVersion
 
 async function execOutput(cmd: string, ...args: string[]): Promise<string> {
