fa72129e2d9f6b1380bf102bff15b531888cbb13
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-07-28 17:33:30 +0200

    Update coursier to 2.1.0-M6-28-gbad85693f

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as tc from '@actions/tool-cache'
 
 let csVersion = core.getInput("version")
 if (!csVersion)
-  csVersion = '2.1.0-M5'
+  csVersion = '2.1.0-M6-28-gbad85693f'
 
 const coursierVersionSpec = csVersion
 
