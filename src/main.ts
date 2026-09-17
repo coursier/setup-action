8bc0a32493c494418415663f1cec04f750e38c35
alexarchambault <7063723+alexarchambault@users.noreply.github.com>
2026-09-17 06:06:56 +0000

    Update Coursier to 2.1.25

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import * as tc from '@actions/tool-cache'
 // but coursier is not published on npm
 import { compareVersions } from 'compare-versions'
 
-const defaultVersion = '2.1.25-M26'
+const defaultVersion = '2.1.25'
 
 const csVersion = core.getInput('version') || defaultVersion
 // Nightly builds are published under the fixed tag "nightly" (no "v" prefix).
